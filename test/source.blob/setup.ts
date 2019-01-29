367351299f66671e77600391d003741da0986029
Zachary Nawar <zanawar@microsoft.com>
2019-01-29 15:53:48 -0800

    Mocked out azure blob storage from tests.

--- test/source.blob/setup.ts
+++ test/source.blob/setup.ts
@@ -1,42 +1,69 @@
 // Compile Contracts
 // Deploy ABI.jsons to Blob
 
-import * as storage from "azure-storage";
+import * as azure from "azure-storage";
 
-export const contractName1: string = "Migrations";
-export const contractName2: string = "TestBed";
+const sinon = require("sinon");
+const proxyquire = require("proxyquire").noCallThru();
 
-export const prepareBlobStorage = (connection: string, containerName: string): Promise<void> =>
-{
-    return new Promise((resolve, reject) => {
-        // Upload Contract .json files to blob storage
-        const blobService = storage.createBlobService(connection);
-        blobService.createContainerIfNotExists(containerName, (err) => {
-          if (err) {
-            reject(err);
-          }
+export const mockedContractNames = [
+  "Migrations",
+  "TestBed"
+];
+export const mockedConnectionString = "connection_string";
+export const mockedContainerName = "testcontracts";
 
-          let dummyAbi1 = { contractName: `${contractName1}` }
-          let dummyAbi2 = { contractName: `${contractName2}` }
-          
-          blobService.createBlockBlobFromText(containerName, contractName1 + ".json", JSON.stringify(dummyAbi1), (err) => {
-            if (err) {
-              reject(err);
-            }
+const blobServiceStub = {
+  createContainerIfNotExists: sinon.stub().callsArgWith(1, null),
+  doesContainerExist: sinon.stub().callsFake((container: string, callback: (err: any, result: azure.BlobService.ContainerResult, response: any) => void) => {
+    callback(
+      null,
+      {
+        exists: container == mockedContainerName
+      } as azure.BlobService.ContainerResult,
+      null);
+  }),
+  listBlobsSegmented: sinon.stub().callsFake((containerName: string, token: any, callback: (err: any, result: azure.BlobService.ListBlobsResult, response: any) => void) => {
+    const fakeResults : azure.BlobService.BlobResult[] = [];
+    for (let i in mockedContractNames) {
+      fakeResults.push({
+        name: mockedContractNames[i] + ".json"
+      } as azure.BlobService.BlobResult);
+    }
 
-            blobService.createBlockBlobFromText(containerName, contractName2 + ".json", JSON.stringify(dummyAbi2), (err) => {
-              if (err) {
-                reject(err);
-              }
+    const result : azure.BlobService.ListBlobsResult = {
+      entries: fakeResults
+    };
 
-              resolve();
-            });
-
-            
-          });
-          
-        });
-        
-    });
+    callback(
+      null,
+      result,
+      null);
+  }),
+  getBlobToText: sinon.stub().callsFake((container: string, blob: string, options: azure.BlobService.GetBlobRequestOptions, callback: (err: any, result: string, blobResult: any, response: any) => void) => {
+    const contractName = blob.replace(".json", "");
+    if (!mockedContractNames.includes(contractName)) {
+      callback(new Error(contractName + ".json is not a valid contract"), "", null, null);
+    }
+    callback(null, JSON.stringify({ contractName: `${contractName}` }), {} as azure.BlobService.BlobResult, null);
+  })
 }
 
+const azureStub = {
+  createBlobService: sinon.stub().callsFake((connString: string) => {
+    if (connString == mockedConnectionString) {
+      return blobServiceStub;
+    }
+
+    throw new Error("Invalid connection string");
+  })
+}
+
+import { AzureBlobContractSource } from "eth-transaction-proxy";
+const AzureBlobContractSourceStub = <typeof AzureBlobContractSource> proxyquire("eth-transaction-proxy/lib/contract-sources/azure-blob/AzureBlobContractSource", {
+  'azure-storage': azureStub
+}).AzureBlobContractSource;
+
+export function createAzureBlobContractSource(connection: string, container: string) {
+  return new AzureBlobContractSourceStub(connection, container);
+}
--- test/source.blob/spec.ts
+++ test/source.blob/spec.ts
@@ -1,96 +1,79 @@
 import "mocha";
+
+
+
 import * as assert from "assert";
-import { AzureBlobContractSource } from "eth-transaction-proxy";
 import * as setup from "./setup";
 
-const containerName = "testcontracts";
-const contractName = setup.contractName1;
-let connectionString = "";
+const contractNames = setup.mockedContractNames;
+const containerName = setup.mockedContainerName;
+const connectionString = setup.mockedConnectionString
+
+import { AzureBlobContractSource } from "eth-transaction-proxy";
+
 let contractSource: AzureBlobContractSource;
 
-// Only actually run blob tests if the environment variable is set to
-if (process.env["TEST_BLOB"] == "1") {
+describe("AzureBlobContractSource", () => {
+  describe("constructor", () => {
 
-    before("Setup blob storage connector", async () => {
-        let env = process.env["AZURE_STORAGE_CONNECTION_STRING"];
-        if (env) {
-            connectionString = env;
-        } else {
-            throw Error("You must setup the environment variable 'AZURE_STORAGE_CONNECTION_STRING' to connect to blob storage for this test suite.");
-        }
-    
-        contractSource = new AzureBlobContractSource(connectionString, containerName);
-        await setup.prepareBlobStorage(connectionString, containerName);
+    it("should accept valid connection string", () => {
+      assert.doesNotThrow(() => {
+        setup.createAzureBlobContractSource(connectionString, containerName);
+      });
     });
-    
-    describe("AzureBlobContractSource", () => {
-        describe("constructor", () => {
-    
-            it("should accept valid connection string", () => {
-                assert.doesNotThrow(() => {
-                    new AzureBlobContractSource(connectionString, containerName);
-                });
-            });
-    
-            it("should reject invalid connection string", () => {
-                const connectionString = 'abcd';
-    
-                assert.throws(() => {
-                    new AzureBlobContractSource(connectionString, containerName);
-                });
-            });
-    
-            it("rejects invalid container", () => {
-    
-                const containerName = 'doesnotexist';
-                contractSource = new AzureBlobContractSource(connectionString, containerName);
-    
-                return contractSource.validateConnection()
-                .then((result) => {
-                    assert.equal(result, false);
-                })
-                .catch((err: Error) => assert.fail(err.message));
-            });
-    
-            it("accepts valid conatiner", async () => {
-              contractSource = new AzureBlobContractSource(connectionString, containerName);
-  
-              assert.equal(await contractSource.validateConnection(), true);
-            });
-        });
-    
-        describe("list", () => {
-            it("should list 2 abi metadatas", () => {
-                return contractSource.list().then((metadatas) => {
-                    assert.equal(metadatas.length, 2);
-                });
-            });
-        });
-    
-        describe("get", () => {
-            it("should get a specific ABI", () => {
-                return contractSource.get(contractName).then((abi) => {
-                    assert(Object.keys(abi).length > 0);
-                });
-            });
-    
-            it("should not find a specific ABI", () => {
-                const contractName = "doesnotexist";
-    
-                let didFail = false;
-                return contractSource.get(contractName).then((abi) => {
-    
-                }).catch((err: Error) => {
-                    didFail = true;
-                }).then(() => {
-                    assert(didFail);
-                });
-            });
-    
-        });
-    });
-    
-} else {
-    console.log("Azure Blob Storage Source testing is disabled.");
-}
 
+    it("should reject invalid connection string", () => {
+      const connectionString = 'abcd';
+
+      assert.throws(() => {
+        setup.createAzureBlobContractSource(connectionString, containerName);
+      });
+    });
+
+    it("rejects invalid container", () => {
+
+      const containerName = 'doesnotexist';
+      contractSource = setup.createAzureBlobContractSource(connectionString, containerName);
+
+      return contractSource.validateConnection()
+      .then((result) => {
+        assert.equal(result, false);
+      })
+      .catch((err: Error) => assert.fail(err.message));
+    });
+
+    it("accepts valid conatiner", async () => {
+      contractSource = setup.createAzureBlobContractSource(connectionString, containerName);
+      assert.equal(await contractSource.validateConnection(), true);
+    });
+  });
+
+  describe("list", () => {
+    it("should list 2 abi metadatas", () => {
+      return contractSource.list().then((metadatas) => {
+        assert.equal(metadatas.length, 2);
+      });
+    });
+  });
+
+  describe("get", () => {
+    it("should retrieve a valid contract", () => {
+      return contractSource.get(contractNames[0]).then((abi) => {
+        assert(Object.keys(abi).length > 0);
+      });
+    });
+
+    it("should fail when trying to get an invalid contract", () => {
+      const contractName = "doesnotexist";
+
+      let didFail = false;
+      return contractSource.get(contractName).then((abi) => {
+
+      }).catch((err: Error) => {
+        didFail = true;
+      }).then(() => {
+        assert(didFail);
+      });
+    });
+  });
+});
